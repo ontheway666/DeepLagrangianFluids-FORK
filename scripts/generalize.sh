5c68759f40cd2423364d05cfdb48542671d6a774
iceki7 <1300953421@qq.com>
2026-01-04 17:27:41 +0800

    overhaul   generalize

--- scripts/generalize.sh
+++ scripts/generalize.sh
@@ -1,34 +1,177 @@
-num_steps=250
-# num_steps=400
-# num_steps=1000
+num_steps=4
+num_steps=20
+# num_steps=50
 # num_steps=100
+num_steps=150
+# num_steps=250
+num_steps=400
+num_steps=500
+num_steps=1000
+# num_steps=1200
+# num_steps=1300
+# num_steps=1500
+# num_steps=2000
+# num_steps=2300
+num_steps=2800
+# num_steps=4000
+
 
 # modelname1=csm400_1111
 # modelname2=csm400_1111
-# modelname1=csm300_1111
-# modelname1=pretrained_model_weights
+# modelname1=csm300
+# modelname1=csm300_6789
 # modelname1=csm200_1111
-modelname1=csm_df300
-# modelname1=csm_df80_sms_1111
-# modelname1=csm_mp300
+# modelname1=default
+# modelname1=csm_df300_sms_1111
 # modelname1=csm350_1111
 
 
+#model sets B
+# modelname1=csm_df300_1111
+# modelname1=csm_mp300
+modelname1=pretrained_model_weights
+# modelname1=csm300_1111
+# modelname1=skdf
+
+
+
+# modelname1=stabledf
+# modelname1=early__csm_mp300
+# modelname1=dup_stable_csm_df300
+
+
+
+#fe(filter extent)
+# modelname1=fe5_csm_df300
+# modelname1=fe7_csm_df300
+# modelname1=fe13_csm_df300
+# modelname1=fe5_noobs_csm_df300
+# modelname1=fe13_noobs_csm_df300
+# modelname1=fe13_noobs_dmcf_csm_df300
+# modelname1=fe18_noobs_csm_df300
+
+
+
+
+
+#dmcf
+# modelname1=dmcf_csm_df300_1234
+# modelname1=dmcf_csm_df300_5678
+# modelname1=dmcf_csm_mp300_5678
+# modelname1=dmcf_csm300_5678
+
+
+# modelname1=RS6__csm_mp300
+# modelname1=RSL3__csm_mp300
+
+
+# modelname1=ContOnPretrained__csm_mp300
+
+
+
+
+
+# scnename=example_static1.5x
+# scnename=example_static_stick
+# scnename=example_still__L
+# scnename=example_still__2L
 scnename=example_static
+# scnename=example_quad
+# scnename=mc_ball_2velx_0602_slow2
+# scnename=mc_ball_2velx_0602_slow2_2
 # scnename=mc_ball_2velx_0602
-scnename=0602_emit
+scnename=mc_ball_2velx_0602_wake
+scnename=mc_ball_2velx_0602_wake_wide
+# scnename=0711
+# scnename=0602_emit
+# scnename=0602_emit_q
+# scnename=0602_emit_q_a
+# scnename=0602_emit_thin
+# scnename=0602_emit_thin_b
+# scnename=emit_big
+# scnename=emit_large
+# scnename=emit_large_myemit
+# scnename=emit_large_1layer
+# scnename=0712
+# scnename=high_fluid_mcvsph-fluid
+# scnename=maxpartnum
+# scnename=rotating_panel
+# scnename=example_propeller
+# scnename=watervessel
+# scnename=watervessel1.4x
+# scnename=watervessel1.5x
+# scnename=watervessel1.13x
+# scnename=watervessel1.06x
+# scnename=watervessel0.8x
+# scnename=watervessel0.7x
+# scnename=watervessel0.9x
+# scnename=wavetower
+# scnename=wavetowerstatic
+scnename=wavetowerstatic1.3x
+# scnename=momeConse
+# scnename=propeller
+# scnename=propellerlarge
+# scnename=streammultiobjs2x
+# scnename=streammultiobjs3x
+# scnename=streammultiobjs3xcut
+# scnename=streammultiobjsHorizon
+# scnename=streammultiobjs2xCut
+# scnename=taylorvortex1.5scale
+# scnename=rotatingpanelstatic
+# scnename=rotatingpanelstatic1.5x
+# scnename=rotatingpanelstatic2x_thin
+scnename=rotatingpanelstatic2x
+# scnename=propellerbiglarge2
+# scnename=propeller2+large2
+
+
+
+
+
+
+
+
+
+# testname=wallmove___
+# testname=____1.5x___
+# testname=____90w___nobox
+testname=___temp___test__rs12___
+#testname=___push680___
+# testname=_____stop650__
+# testname=____onlyrigid__
+# testname=____horizon__
+# testname=__ONLYNPZ_____
+testname=__wait500__
+testname=__wait__inf__
+# testname=__cont_on_stable_400__
+# testname=_____notower___
+# testname=___80w__onlynpz___
+testname=____nopanel___
+# testname=____exportall___
+testname=____cp_____
+testname=____FullProcess___
+# testname=____norotating___
+testname=__Cont770__
+# testname=_______
+# testname=_____rmLeft__v0.75__
+
+
+lize=lize
+
+
+
 
 ckpt=_50k
 syncdir=/w/cconv-dataset/sync/
-testname=_
-filename=$testname$modelname1$ckpt$scnename
+
+filename=$lize$testname$modelname1$scnename
+
+python prms.py --weights $modelname1.h5 \
+                --type 1 \
+                --scene $scnename.json || exit 1
 
 
 rm -r $syncdir$filename
-
-
-
-
 ./run_network.py --weights $modelname1.h5 \
                 --scene $scnename.json \
                 --output $syncdir$filename \
@@ -39,13 +182,8 @@ rm -r $syncdir$filename
 
 
 cd $syncdir
-rm $filename.zip
-zip -rq  $filename.zip \
-         $filename
-
-
-
-rm -r $filename
+# rm $filename.zip
+# zip -rq  $filename.zip $filename && rm -r $filename
 
 
 cd /workspace/DeepLagrangianFluids-FORK/scripts/
--- scripts/generalizemm.sh
+++ scripts/generalizemm.sh
@@ -1,26 +1,175 @@
-num_steps=10
-# num_steps=250
+num_steps=3
+num_steps=5
+# num_steps=10
+num_steps=250
 # num_steps=400
-# num_steps=1000
+num_steps=505
+# num_steps=600
+# num_steps=800
+num_steps=1000
+# num_steps=1100
+# num_steps=1200
+# num_steps=1400
+num_steps=1500
+# num_steps=1700
+# num_steps=2000
+# num_steps=2300
+# num_steps=2500
+num_steps=2800
+# num_steps=5000
+
+
 
 modelname1=csm_df300_1111
 
+
+
+
 scnename=mc_ball_2velx_0602
 # scnename=example_static
+# scnename=emit_large
+# scnename=emit_large_myemit
+# scnename=emit_large_myemit_slope
+# scnename=emit_large_myemit_slope_2
+# scnename=emit_large_myemit_slope_3
+# scnename=emit_large_myemit_slope_move
+# scnename=emit_large_myemit_slope_move_x+
+# scnename=example_long2z+2
+# scnename=example_still__L
+# scnename=example_still__2L
+# scnename=example_propeller
+# scnename=maxbox
+# scnename=high_fluid_mcvsph-fluid
+# scnename=rotating_panel
+# scnename=watervessel
+# scnename=watervessel1.13x
+# scnename=wavetower
+scnename=wavetowerstatic
+# scnename=propellerlarge
+# scnename=propellerlarge1.5x
+# scnename=propeller2large
+# scnename=propeller2large2
+# scnename=propeller2large21.5x
+# scnename=taylorvortex
+# scnename=streammultiobjs3x
+# scnename=streammultiobjs3xcut
+# scnename=streammultiobjs3xcutbunny12
+# scnename=streammultiobjs3xcutbunny12-1.5x
+# scnename=streammultiobjs2x
+# scnename=streammultiobjsHorizon
+# scnename=streammultiobjs2xCut
+# scnename=rotatingpanelstatic
+# scnename=taylorvortex1.5scale
+# scnename=propellerbiglarge2
+# scnename=propeller2+large2
+# scnename=propeller2+large2fill
+
+
+
+
+
+
+
+
+
 
 
 testname=emax_b_
-testname=emin_b_
-testname=minmaxmin_b_
-testname=temp__
-# testname=emin
+# testname=CP__emax_b_
+# testname=emax_b_1.5x__
+# testname=cp__emin_b_
+# testname=cp__emax_b_
+# testname=minmaxmin_b_
+# testname=area_n10tn2
+# testname=areamaxzt0_
+# testname=area_n10tn2
+# testname=emin__period0.75__
+# testname=sus320__
+# testname=emin__sus150_stable0.95__0.5rot__
+# testname=emin__rot0.5__
+# testname=emax__rot0.5__
+# testname=emin__rot.125__
+# testname=emin__acc___
+testname=emin__
+# testname=emin__static80__
+# testname=emin__nopush__
+# testname=horizon_emin__
+# testname=emin__moveoutpart___
+# testname=emax__restrict__acc___
+# testname=ONLYNPZ__emin__
+# testname=ONLYNPZ__q0.5___
+# testname=quanti0.5____period0.6__startpush650___
+# testname=emin__test___
+# testname=maxmin__
+# testname=emin__period0.5__
+# testname=quanti0.5____acc___
+# testname=quanti0.5____period0.75__wait500___
+# testname=quanti0.5____period0.75__push680__
+# testname=q0.5__0.2_line__
+# testname=q0.5__0.2_osc__
+# testname=q0.7__0.2__osc8__
+# testname=q1.2__0.5__osc6__
+# testname=q5__FullProcess___
+# testname=quanti0.65____period0.75__
+# testname=emin__rot0.25__holprop__
+# testname=emin__rot0.25__
+# testname=sus_stable__
+# testname=emin_stable0.95__
+# testname=emin_stable0.5__
+# testname=emin__NoProp__
+# testname=emin__rot0.5__restrictUp__
+# testname=emin__1.5rot__
+# testname=eminC__
+# testname=emin__exactarg__
 # testname=pw_max_ 
 # testname=pw_min_
 # testname=eminmaxmin_d7_
 # testname=emin4_
 # testname=pw_min4_
 # testname=pw_max4_
-# testname=_
+# testname=___maxmin___
+# testname=___min___
+# testname=0602__switch2__
+# testname=temp____
+# testname=temp__slope1_v3__
+# testname=quanti__1.15-300start__
+# testname=quanti__1.15-clampn2__
+# testname=quanti__cp_1.25curve__
+# testname=quanti__tau1.25t0.7curve__
+# testname=quanti__tau1.25t0.5line__
+# testname=area_linear__myemit_
+# testname=quanti__1.25line__
+# testname=quanti__0.75____
+# testname=quanti__0.5____
+# testname=quanti__0.5t0.2line____
+# testname=quanti__0.5t0.75line____
+# testname=quanti__0.2t1line____
+# testname=quanti__0.25____
+# testname=quanti__0____
+# testname=pw___min__
+# testname=pw___max__
+# testname=temp____random_linear___
+# testname=temp__myeit__
+# testname=area__emin1.2t0.5and0.5___
+# testname=area__0.8t0.5and0.5___
+# testname=area__1.2t0.2and0.5___
+# testname=area__maxmin___
+# testname=area__0.7and0.5__
+# testname=ONLYNPZ__quanti__tau1.25t0.2curve__
+# testname=ONLYNPZ__quanti_1.25_masked___
+# testname=ONLYNPZ__quanti_0.625___
+# testname=ONLYNPZ__emaxb__CP_
+# testname=exact_0.75__
+# testname=exact_0.8__
+# testname=_emin___90w___nobox
+# testname=fixcoff__q1.25t0.2__
+testname=__Cont770__q3_p75__
+# testname=__Cont770__emin__
+
+
+
+
+
 
 
 
@@ -31,11 +180,13 @@ outputdir=$syncdir$filename
 
 #同时加载多个模型，施加修正
 
-rm -r $outputdir
+python prms.py --weights $modelname1.h5 \
+                --type 1 \
+                --prm_mix 1\
+                --scene $scnename.json || exit 1
+                
 
-
-
-
+# rm -r $outputdir
 ./run_network.py --weights $modelname1.h5 \
                 --scene $scnename.json \
                 --prm_mix 1\
@@ -48,12 +199,9 @@ rm -r $outputdir
 
 cd $syncdir
 
-rm  $filename.zip
 
-zip -rq  $filename.zip \
-         $filename
-
-rm -r $filename
+# rm  $filename.zip
+# zip -rq  $filename.zip $filename && rm -r $filename
 
 
 cd /workspace/DeepLagrangianFluids-FORK/scripts/
